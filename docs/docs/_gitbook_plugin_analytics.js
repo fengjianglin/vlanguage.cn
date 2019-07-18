ba95eb2f5b4c05d6b212ebaade86620bcc769f2a
Your Name <you@example.com>
2019-07-18 14:59:59 +0800

    baidu tj

--- docs/docs/_gitbook_plugin_analytics.js
+++
@@ -1,9 +0,0 @@
-if ('623969e39431ed8038c14723604877f6') {
-	var _hmt = _hmt || [];
-	(function() {
-	  var hm = document.createElement("script");
-	  hm.src = "https://hm.baidu.com/hm.js?" + '623969e39431ed8038c14723604877f6';
-	  var s = document.getElementsByTagName("script")[0];
-	  s.parentNode.insertBefore(hm, s);
-	})();
-}
